e3020ba4a5cf7ea05e63b0f7cfd74dd398a4ef0d
scrappyjs666 <82704685+scrappyjs666@users.noreply.github.com>
2022-05-20 11:19:33 +0300

    fixed eslint error

--- src/api/network.js
+++ src/api/network.js
@@ -9,12 +9,10 @@ export const getApiResource = async (url) => {
       },
     );
     if (!res.ok) {
-      console.error('Could not fetch.', res.status);
       return false;
     }
     return await res.json();
   } catch (error) {
-    console.error('Could not fetch.', error.message);
     return false;
   }
 };
