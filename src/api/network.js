70faf6d18b205bf79517e86629bde284e8536c23
scrappyjs666 <82704685+scrappyjs666@users.noreply.github.com>
2022-05-20 11:22:48 +0300

    fixed headers request

--- src/api/network.js
+++ src/api/network.js
@@ -1,13 +1,6 @@
 export const getApiResource = async (url) => {
   try {
-    const res = await fetch(
-      url,
-      {
-        headers: {
-          authorization: 'token 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',
-        },
-      },
-    );
+    const res = await fetch(url);
     if (!res.ok) {
       return false;
     }
